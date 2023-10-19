1d414ba6601926b642fd7ececa14f3d7c28a8d04
Jeff-Moreau <119882736+Jeff-Moreau@users.noreply.github.com>
2023-10-18 22:35:00 -0400

    updates before bed

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private AudioSource soundSource;
     [SerializeField] private AudioClip jumpSound;
     [SerializeField] private AudioClip pickupSound;
+    [SerializeField] private GameObject pickupSpot;
 
     private bool doubleJump = false;
     private bool isGrounded;
@@ -23,6 +24,7 @@ public class PlayerMovement : MonoBehaviour
         {
             myBody.AddForce(Vector2.up * jumpForce);
             soundSource.PlayOneShot(jumpSound);
+
         }
         else if (Input.GetKeyDown(KeyCode.Space) && !isGrounded && doubleJump)
         {
@@ -35,6 +37,12 @@ public class PlayerMovement : MonoBehaviour
         {
             myBody.AddForce(Vector2.up * controllerJumpForce);
             soundSource.PlayOneShot(jumpSound);
+        }
+        else if (Input.GetButtonDown("Jump") && !isGrounded && doubleJump)
+        {
+            myBody.AddForce(Vector2.up * controllerJumpForce);
+            soundSource.PlayOneShot(jumpSound);
+            doubleJump = false;
         }
     }
 
@@ -66,6 +74,10 @@ public class PlayerMovement : MonoBehaviour
     {
         if (collision.gameObject.layer == 7)
         {
+            if (pickupSpot.activeSelf == true)
+            {
+                pickupSpot.SetActive(false);
+            }
             doubleJump = true;
             soundSource.PlayOneShot(pickupSound);
         }
